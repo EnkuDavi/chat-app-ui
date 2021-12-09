ad9fbb006c2c151980f3ce67ffe984eeec92eb79
enkudavi <ekopur05@gmail.com>
2021-12-09 13:42:48 +0700

    upload file chat app with flutter

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Home'),
+    );
+  }
+}
