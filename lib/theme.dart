3807c04801d6449ed9aa4f66bf2e12b4ab38768b
enkudavi <ekopur05@gmail.com>
2021-12-09 14:47:04 +0700

    create variable for text style

---
+++ lib/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Color blueColor = Color(0xff1F8DF5);
+Color whiteColor = Color(0xffFFFFFF);
+Color lightBlue = Color(0xff9ED0FF);
+Color blackColor = Color(0xff2C3A59);
+Color grey = Color(0xff808BA2);
+
+TextStyle textTitle =
+    TextStyle(color: blackColor, fontWeight: FontWeight.w500, fontSize: 16);
+
+TextStyle subtitle =
+    TextStyle(color: blackColor, fontWeight: FontWeight.w300, fontSize: 14);
